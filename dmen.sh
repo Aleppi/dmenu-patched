6642eadf52f473b4c11b9f53433adbc4c8142d63
Aleppi <hectortib@gmail.com>
2018-05-15 15:58:12 +0200

    Fixed font

--- dmen.sh
+++ dmen.sh
@@ -2,4 +2,4 @@
 
 . "${HOME}/.cache/wal/colors.sh"
 
-dmenu_run -i -l 10 -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" -fn "fixed:style=regular:pixelsize=12"
+dmenu_run -i -l 10 -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" -fn "Terminus:pixelsize=12"
